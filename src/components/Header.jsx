44e50287fff68016b14cd1d3804ccf210aaf0cbe
JCat <andryxakulishow@gmail.com>
2020-10-03 13:52:11 +0300

    my project on react

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://pbs.twimg.com/media/BJWk3OdCcAATCbB.jpg" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://docs.microsoft.com/ru-ru/windows/uwp/design/controls-and-patterns/images/image_licorice.jpg" />
+            </div>
+            <div>
+                ava + description
+        </div>
+            <div>
+                My post
+          <div>
+                    New post
+          </div>
+                <div>
+                    <div>
+                        post 1
+            </div>
+                    <div>
+                        post 2
+            </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
